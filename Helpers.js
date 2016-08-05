3b0e9de19e673bb1f2f0c1cd91748cbf021d6285
Adrian Salgado <adrian@schibsted.cl>
2016-08-05 12:18:46 -0300

    develop - Will improve ignore to make a decent npm package

--- Helpers.js
+++ Helpers.js
@@ -3,27 +3,11 @@ module.exports = Helpers = (() => {
   'use strict';
   const Helpers = {};
 
+  /* 
   Helpers.login = function login(g) {
-    g.wait(500);
-    g.goToSecure('login');
-
-    // Fill form
-    let email = g.getElementById('email_input');
-    email.sendKeys('prepaid5@blocket.se');
-
-    let password = g.getElementById('password_input');
-    password.sendKeys('123123123');
-
-    g.wait(500);
-
-    // Submit form
-    password.submit();
+    // Add your helper code here.
   }
-
-  Helpers.logout = function logout(g) {
-    g.getElementByClassName('header-userLogout').click();
-    g.wait(500);
-  }
+  */
 
   return Helpers;
 })();
